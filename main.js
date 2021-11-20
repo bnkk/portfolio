e02a0150f8f019062fc2abb898538ecac69d1bcd
Braydon Turner <braydonturn@gmail.com>
2021-11-20 01:06:31 -0500

    spring cleaning

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import "./style.css";
 import * as THREE from "three";
 import { throttle } from "lodash-es";
 // create adjustment for window resize
-//import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 // create scene, camera and renderer
 const scene = new THREE.Scene();
@@ -26,25 +25,6 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
-
-//renderer.render(scene, camera);
-
-/*const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(20,20,20)
-
-const ambientLight = new THREE.AmbientLight(0xffffff)
-ambientLight.castShadow = true;
-scene.add(pointLight, ambientLight)
-
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200,50);
-scene.add(lightHelper, gridHelper)
-
-ambientLight.shadow.mapSize.width = 512;
-ambientLight.shadow.mapSize.height = 512;
-ambientLight.shadow.camera.near = 0.5;
-ambientLight.shadow.camera.far = 500;
-*/
 
 // set background to dark blue
 scene.background = new THREE.Color(0x030e2f);
@@ -73,22 +53,6 @@ scene.add(plane);
 plane.rotation.set(1.57, 0, 0);
 plane.position.set(0, -10, 0);
 
-/*
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({color: 0xffffff});
-  const star = new THREE.Mesh(geometry,material);
-
-  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
-
-  star.position.set(x,y,z);
-  scene.add(star)
-};
-
-Array(200).fill().forEach(addStar)
-*/
-//const controls = new OrbitControls(camera,renderer.domElement);
-
 // create dim ambient light
 const ambientLight = new THREE.AmbientLight(0x404040);
 scene.add(ambientLight);
@@ -106,10 +70,6 @@ light.shadow.mapSize.height = 2048;
 light.shadow.camera.near = 0.5;
 light.shadow.camera.far = 500;
 
-//const lightHelper = new THREE.SpotLightHelper(light)
-//const gridHelper = new THREE.GridHelper(200, 50)
-//scene.add(lightHelper)
-
 // when window is resized, change renderer size and camera fov
 window.addEventListener(
   "resize",
@@ -120,39 +80,12 @@ window.addEventListener(
       camera.aspect = width / height;
       camera.updateProjectionMatrix();
       renderer.setSize(width, height);
-      //setCanvasDimensions(renderer.domElement, width, height);
     },
     { trailing: true }
   )
 );
-/*
-document.addEventListener("mousedown", onDocumentMouseDown, false);
-var projector = new THREE.Projector();
-function onDocumentMouseDown(event) {
-  event.preventDefault();
-  var vector = new THREE.Vector3(
-    (event.clientX / window.innerWidth) * 2 - 1,
-    -(event.clientY / window.innerHeight) * 2 + 1,
-    0.5
-  );
-  projector.unprojectVector(vector, camera);
-  var ray = new THREE.Ray(
-    camera.position,
-    vector.subSelf(camera.position).normalize()
-  );
-  var intersects = ray.intersectsObjects(cube);
-
-  if (intersects.length > 0) {
-  }
-}
-*/
 
 let targetList = [cube];
-
-/*
-var raycaster = new THREE.Raycaster();
-var mouse = new THREE.Vector2();
-*/
 
 const interval = setInterval(function () {
   let text = document.getElementById('footer-text');
@@ -215,25 +148,6 @@ function clicker(obj) {
 
   document.getElementById("secondPage").style.animation =
     "fadeIn 1s forwards 3s";
-/*
-  setTimeout(() => {
-    renderer.domElement.addEventListener('dblclick', null, false);
-    scene = null;
-    camera = null;
-    empty(this.modelContainer);
-  }, 500); */
-
-
-  /*let div = document.querySelector('#secondPage');
-  const newSection = document.createElement("section");
-  const newHeader = document.createElement("h1");
-  const newContent = document.createTextNode("test");
-
-  newHeader.appendChild(newContent);
-  newSection.appendChild(newHeader);
-
-  div.appendChild(newSection);
-  */
 
   changePage();
 }
@@ -264,10 +178,6 @@ function animate() {
   camera.position.set(27, 7, -5);
   camera.rotation.set(179.6, 0.9, 90.7);
 
-  //controls.update();
-
-  //document.getElementById("footer-text").style.animation =
-  //  "bounce 1s forwards 15s";
   renderer.render(scene, camera);
 }
 
